91a7059e755a8b2f8a10eb0eb07bad11613056fb
Lari Bernardes <bernardesl171@gmail.com>
2026-01-27 19:26:54 -0300

    exercício

---
+++ Aula06/exercício01.py
@@ -0,0 +1,7 @@
+#Imprimir números#
+
+contador = 0
+
+while contador < 11:
+    print(contador)
+    contador += 1
---
+++ Aula06/exercício05.py
@@ -0,0 +1,6 @@
+#Criar um programa que pergunta o nome#
+
+nome = input('Digite o seu nome: ');
+
+while nome == 
+    print(nome)
